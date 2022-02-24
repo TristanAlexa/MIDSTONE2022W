270196a4d56d93636fddc21a286eee819cf87f67
Nikki Liu <55368578+LastLightStudios@users.noreply.github.com>
2022-02-23 19:12:16 -0700

    Removed unnecessary functions from Player Class

--- Framework/Player.h
+++ Framework/Player.h
@@ -13,12 +13,7 @@ private:
 	Vec3 pos;
 	Vec3 vel;
 	Vec3 accel;
-	Vec3 force;
 	float gravity;
-	float mass;
-
-	float rotationZ;
-	float radius;
 
 	SDL_Surface* image;
 	SDL_Texture* texture;
@@ -27,20 +22,13 @@ private:
 
 public:
 	Player();
-	Player(Vec3 pos_, Vec3 vel_, Vec3 accel_, float mass_);
+	Player(Vec3 pos_, Vec3 vel_, Vec3 accel_);
 	~Player();
 
 	void isMoving(bool TorF) { is_moving = TorF; }
 	//void isGrounded(bool TorF) { is_grounded = TorF; }
 	bool isGrounded();
 	void Update(float deltaTime);
-
-	//Force
-	inline void ApplyForce(Vec3 force_) { force = force_; }
-	inline void ApplyForceX(float Fx) { force.x = Fx; }
-	inline void ApplyForceY(float Fy) { force.y = Fy; }
-	inline void UnsetForceX() { force.x = 0.0f; }
-	inline void UnsetForceY() { force.y = 0.0f; }
 
 	//Getter-Setters
 	Vec3 getPos() { return pos; }
@@ -50,10 +38,6 @@ public:
 	void setVelocityY(float Vy) { vel.y = Vy; }
 	void setPosition(Vec3 pos_) { pos = pos_; }
 	Vec3 getAcceleration() { return accel; }
-	float getRadius() { return radius; }
-	float getMass() { return mass; }
-	float getRotationZ() { return rotationZ; }\
-	inline void setMass(float mass_) { mass = mass_; }
 	inline void setGravity(float gravity_) { gravity = gravity_; }
 
 	void setImage(SDL_Surface* image_) { image = image_; }
